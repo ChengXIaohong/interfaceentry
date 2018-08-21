2a66c5a6e20a7944d7769b0dc25db6fda11f932c
程小洪 <chengxiaohong@iwubida.com>
2018-08-21 17:18:19 +0800

    前端完善与异常捕获以及展示

---
+++ src/main/java/com/interfaceentry/interfaceentry/controller/FinalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.interfaceentry.interfaceentry.controller;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 全局异常Controller
+ *
+ * @author chengxiaohong coax@outlook.it
+ * @create 2018-07-03 12:07
+ **/
+
+public class FinalExceptionHandler{
+
+    /**
+     * 通用异常处理
+     * @param t
+     * @param request
+     * @param response
+     */
+    @ExceptionHandler(value = Throwable.class)
+    @ResponseBody
+    public String exceptionHandler(Throwable t, HttpServletRequest request, HttpServletResponse response) {
+        Map<String ,Object> statusMap = new HashMap<>();
+        statusMap.put("status", "error");
+        Map<String , String> errorMessageMap = new HashMap<>();
+        errorMessageMap.put("msg" , t.getMessage());
+        errorMessageMap.put("date" , new Date().toString());
+        errorMessageMap.put("path" , request.getRequestURI());
+        statusMap.put("errorInfo",errorMessageMap);
+        return JSON.toJSONString(statusMap);
+    }
+}
